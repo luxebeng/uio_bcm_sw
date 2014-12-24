3d89f26fbab26872e3c2d7fce0003a149419c902
benliu <benliu@ngsrx-build51.juniper.net>
2014-12-24 00:45:45 -0800

    clean the dummy files

--- kernel/.tmp_versions/uio_bcm_sw.mod
+++
@@ -1,2 +0,0 @@
-/volume/ngsrx-dev-store/sandboxes/benliu/bcm_uio/kernel/uio_bcm_sw.ko
-/volume/ngsrx-dev-store/sandboxes/benliu/bcm_uio/kernel/uio_bcm_sw.o
--- kernel/.uio_bcm_sw.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/volume/ngsrx-dev-store/sandboxes/benliu/bcm_uio/kernel/uio_bcm_sw.ko := cp /volume/ngsrx-dev-store/sandboxes/benliu/bcm_uio/kernel/uio_bcm_sw.ko.unsigned /volume/ngsrx-dev-store/sandboxes/benliu/bcm_uio/kernel/uio_bcm_sw.ko
